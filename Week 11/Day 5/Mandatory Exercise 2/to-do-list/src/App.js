5670e235dfcccd70ec041b0d8dc953072c2314d5
Nwosu Chukwudalu <chukwudalunwosu10@gmail.com>
2023-06-18 10:19:18 +0300

    To-do List

---
+++ Week 11/Day 5/Mandatory Exercise 2/to-do-list/src/App.js
@@ -0,0 +1,129 @@
+import './App.css';
+import React, { Component } from 'react'
+
+
+const Todos = ({todos, deleteTodo}) => {
+
+
+  console.log(todos);
+
+
+
+  const todoList = todos.length ? (
+    todos.map(todo => {
+      return (
+        <div className="collection-item" key={todo.id}>
+          <span onClick={() => {deleteTodo(todo.id)}}>{todo.content}</span>
+        </div>
+      )
+    })
+  ) : (
+    <p className="center">You have nothing To do anymore</p>
+  );
+
+  return (
+    <div className="todos collection">
+      {todoList}
+    </div>
+  )
+}
+
+class AddTodo extends Component {
+  state = {
+    content: ''
+  }
+
+
+  handleChange = (e) => {
+    this.setState({
+      content: e.target.value
+    });
+  }
+
+
+  handleSubmit = (e) => {
+
+    e.preventDefault();
+    
+
+    this.props.addTodo(this.state);
+    this.setState({
+      content: ''
+    })
+
+  }
+
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <label>Add something to your to-do list:</label>
+          <input type="text" onChange={this.handleChange} value={this.state.content} />
+        </form>
+      </div>
+    )
+  }
+}
+
+
+
+class App extends Component {
+  
+  state = {
+
+     todos: [
+  
+     ]
+  }
+
+
+
+
+   componentDidMount() {
+    
+    
+   }
+
+  
+
+  deleteTodo = (id) => {
+    const todos = this.state.todos.filter(todo => {
+      return todo.id !== id
+    });
+
+
+
+
+    this.setState({
+      todos
+    });
+
+  }
+
+
+
+  addTodo = (todo) => {
+    todo.id = Math.random();
+    let todos = [...this.state.todos, todo];
+    this.setState({
+      todos
+    });
+
+
+
+  }
+  
+  
+  render() {
+    return (
+      <div className="todo-app container">
+        <h1 className="center red-text">To-do List</h1>
+        <Todos todos={this.state.todos} deleteTodo={this.deleteTodo} />
+        <AddTodo addTodo={this.addTodo} />
+      </div>
+    );
+  }
+}
+
+export default App;
